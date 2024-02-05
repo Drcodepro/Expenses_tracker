aae93e9ed4dfe04c5847f8c7ff5d85580a5856a9
Drcodepro <drrahangdale01@gmail.com>
2024-02-05 16:35:26 +0530

    added some real values instead of hardcoded values in the expenses.

--- sharpner_expenses_app/src/component/ExpenseItem.js
+++ sharpner_expenses_app/src/component/ExpenseItem.js
@@ -1,12 +1,15 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(){
+    const ExpenseDate = new Date(2024,1,5);
+    const ExpenseTitle = 'Car Insurance';
+    const ExpensePrice = 254.3;
     return( 
     <div className="expense-item">
-        <div>March 28th 2021</div>
+        <div>{ExpenseDate.toDateString()}</div>
         <div className='expense-item__description'>
-            <h2>Car Insurance</h2>
-            <div className='expense-item__price'>$294.67</div>
+            <h2>{ExpenseTitle}</h2>
+            <div className='expense-item__price'>${ExpensePrice}</div>
         </div>
     </div>
     );
