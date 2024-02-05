93f55c5e63af9aa696fac99dfc70726e076e66da
Drcodepro <drrahangdale01@gmail.com>
2024-02-05 19:40:04 +0530

    splited the ExpenseDate component into another file and added some styling

--- sharpner_expenses_app/src/component/ExpenseItem.js
+++ sharpner_expenses_app/src/component/ExpenseItem.js
@@ -1,18 +1,12 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props){
-   // extracting the month day and year from the Date object
-    const month = props.date.toLocaleString('en-us',{month:'long'})
-    const day = props.date.toLocaleString('en-us',{day:'2-digit'})
-    const year = props.date.getFullYear();
-
+    
     return( 
     <div className="expense-item">
-        <div>
-            <div>{day}</div>
-            <div>{month}</div>
-            <div>{year}</div>
-        </div>
+        <ExpenseDate date={props.date}/>  
+
         <div className='expense-item__description'>
             <h2>{props.tital}</h2>
             <h2>{props.location}</h2>
