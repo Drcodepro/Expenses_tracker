9e1730b3136bce087dbb266ca335fb263d7dd45b
Drcodepro <drrahangdale01@gmail.com>
2024-02-05 17:18:22 +0530

    used props to display the different values in different components and also used loops to calling component multiple times.

--- sharpner_expenses_app/src/App.js
+++ sharpner_expenses_app/src/App.js
@@ -1,9 +1,25 @@
 import ExpenseItem from "./component/ExpenseItem";
 function App() {
+  const expenses = [
+    { tital: "Car insurance", date: new Date(2024, 1, 2), amount: 76382,location:"balaghat"},
+    { tital: "bithday party", date: new Date(2024, 1, 7), amount: 376 ,location:"balaghat" },
+    { tital: "Traveling", date: new Date(2024, 1, 15), amount: 10932,location:"balaghat" },
+    { tital: "collage fee", date: new Date(2024, 1, 29), amount: 8761,location:"balaghat" },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      {
+       expenses.map((element,index) => {
+        return <ExpenseItem key={index}
+        tital={element.tital}
+        date={element.date}
+        amount={element.amount}
+        location ={element.location}
+      ></ExpenseItem>
+       })
+      }
+      
     </div>
   );
 }
--- sharpner_expenses_app/src/component/ExpenseItem.js
+++ sharpner_expenses_app/src/component/ExpenseItem.js
@@ -1,15 +1,14 @@
 import './ExpenseItem.css';
 
-function ExpenseItem(){
-    const ExpenseDate = new Date(2024,1,5);
-    const ExpenseTitle = 'Car Insurance';
-    const ExpensePrice = 254.3;
+function ExpenseItem(props){
+   
     return( 
     <div className="expense-item">
-        <div>{ExpenseDate.toDateString()}</div>
+        <div>{props.date.toDateString()}</div>
         <div className='expense-item__description'>
-            <h2>{ExpenseTitle}</h2>
-            <div className='expense-item__price'>${ExpensePrice}</div>
+            <h2>{props.tital}</h2>
+            <div className='expense-item__price'>${props.amount}</div>
+            <h2>{props.location}</h2>
         </div>
     </div>
     );
