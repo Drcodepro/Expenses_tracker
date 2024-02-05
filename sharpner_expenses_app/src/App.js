5254ae9bb855a4b05998f2f223014bdb3d438a54
Drcodepro <drrahangdale01@gmail.com>
2024-02-05 19:28:36 +0530

    made some changes in the expenseItem component

--- sharpner_expenses_app/src/App.js
+++ sharpner_expenses_app/src/App.js
@@ -1,16 +1,17 @@
 import ExpenseItem from "./component/ExpenseItem";
 function App() {
   const expenses = [
-    { tital: "Car insurance", date: new Date(2024, 1, 2), amount: 76382,location:"balaghat"},
-    { tital: "bithday party", date: new Date(2024, 1, 7), amount: 376 ,location:"balaghat" },
-    { tital: "Traveling", date: new Date(2024, 1, 15), amount: 10932,location:"balaghat" },
-    { tital: "collage fee", date: new Date(2024, 1, 29), amount: 8761,location:"balaghat" },
+    { tital: "Car insurance", date: new Date(2024, 1, 2), amount: 76382,location:"bhopal"},
+    { tital: "bithday party", date: new Date(2024, 1, 7), amount: 376 ,location:"new delhi" },
+    { tital: "Traveling", date: new Date(2024, 1, 15), amount: 10932,location:"goa" },
+    { tital: "collage fee", date: new Date(2024, 1, 29), amount: 8761,location:"sagar" },
   ];
   return (
     <div>
       <h2>Let's get started!</h2>
       {
-       expenses.map((element,index) => {
+       expenses.map((element,index) => { // Map() function for loop , we uses map() because jsx can accept the only valid expressions that returns any value
+
         return <ExpenseItem key={index}
         tital={element.tital}
         date={element.date}
--- sharpner_expenses_app/src/component/ExpenseItem.js
+++ sharpner_expenses_app/src/component/ExpenseItem.js
@@ -1,14 +1,22 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props){
-   
+   // extracting the month day and year from the Date object
+    const month = props.date.toLocaleString('en-us',{month:'long'})
+    const day = props.date.toLocaleString('en-us',{day:'2-digit'})
+    const year = props.date.getFullYear();
+
     return( 
     <div className="expense-item">
-        <div>{props.date.toDateString()}</div>
+        <div>
+            <div>{day}</div>
+            <div>{month}</div>
+            <div>{year}</div>
+        </div>
         <div className='expense-item__description'>
             <h2>{props.tital}</h2>
-            <div className='expense-item__price'>${props.amount}</div>
             <h2>{props.location}</h2>
+            <div className='expense-item__price'>${props.amount}</div>
         </div>
     </div>
     );
