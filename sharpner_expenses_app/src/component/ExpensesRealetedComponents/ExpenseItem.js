6af8fbfe8e67175131ac74ddf0fc81d2601d6953
Drcodepro <drrahangdale01@gmail.com>
2024-02-21 22:13:52 +0530

    added the button  next to the every expense card that will change the expense amount with 100$ when we click on the button.

--- sharpner_expenses_app/src/component/ExpensesRealetedComponents/ExpenseItem.js
+++ sharpner_expenses_app/src/component/ExpensesRealetedComponents/ExpenseItem.js
@@ -1,30 +1,23 @@
-import './ExpenseItem.css';
-import ExpenseDate from './ExpenseDate';
-import Card from '../UiRelatedComponent/Card';
-import React,{useState} from "react";
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UiRelatedComponent/Card";
+import React, { useState } from "react";
 
-export default function ExpenseItem(props){
-
-       const [Delete,changeDelete]=useState(true)
-    let handleDelete=()=>{
-        let val =!Delete;
-       changeDelete(val)
-    }
-        if(Delete){
-        return( 
-            
-            <Card className="expense-item">
-                <ExpenseDate date={props.date}/>  
-                <div className='expense-item__description'>
-                    <h2>{props.location}</h2>
-                    <h2>{props.tital}</h2>
-                    <div className='expense-item__price'>${props.amount}</div>
-                    <button onClick={handleDelete}>Dellete Expense</button>
-                </div>
-            </Card>
-            );
-        }
-    else{
-        return null;
-    }
+export default function ExpenseItem(props) {
+  // useState()  hook
+  const [Amount, changeExpense] = useState(props.amount);
+  let handleExpense = () => {
+    changeExpense(100);
+  };
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.location}</h2>
+        <h2>{props.tital}</h2>
+        <div className="expense-item__price">${Amount}</div>
+        <button onClick={handleExpense}>Change Expense</button>
+      </div>
+    </Card>
+  );
 }
