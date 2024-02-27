f546b6fdaaa3c8cc9bf69b56663335ed487d9ee8
Drcodepro <drrahangdale01@gmail.com>
2024-02-28 00:16:39 +0530

    made some hoisting related changes for better user experiance....

--- sharpner_expenses_app/src/component/ExpensesRealetedComponents/ExpenseItem.js
+++ sharpner_expenses_app/src/component/ExpensesRealetedComponents/ExpenseItem.js
@@ -1,22 +1,16 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UiRelatedComponent/Card";
-import React, { useState } from "react";
 
 export default function ExpenseItem(props) {
-  // useState()  hook
-  const [Amount, changeExpense] = useState(props.amount);
-  let handleExpense = () => {
-    changeExpense(100);
-  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.location}</h2>
         <h2>{props.title}</h2>
-        <div className="expense-item__price">${Amount}</div>
-        <button onClick={handleExpense}>Change Expense</button>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </Card>
   );
